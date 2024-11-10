16ef836041bbb1685764722dead43c69d51389ba
Om Chauhan <omchauhan@Oms-MacBook-Pro.local>
2024-11-10 18:55:42 +0530

    initialized project

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className='flex justify-center items-center min-h-screen'>
+      <h1 className='text-5xl'>Landing Page</h1>
+    </div>
+  );
+}
